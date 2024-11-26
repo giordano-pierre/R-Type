b6fbf2dc61b01baa36ba7e4f352f1f6127ef27b7
giordano <pierre.giordano@epitech.eu>
2024-11-26 17:43:59 +0100

    [UPD] gui gif

--- client/main.cpp
+++ client/main.cpp
@@ -1,20 +1,18 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <iostream>
 #include <vector>
 #include <string>
+#include <iostream>
 
 class Gui
 {
 public:
-    Gui(const std::string& framesPath, int frameCount, int frameRate)
-        : m_frameCount(frameCount), m_frameRate(frameRate), m_currentFrame(0)
+    Gui(const std::string& framesPath, int frameCount, int frameDurationMs)
+        : m_frameCount(frameCount), m_frameDuration(sf::milliseconds(frameDurationMs)), m_currentFrame(0)
     {
         for (int i = 0; i < frameCount; ++i)
         {
             sf::Texture texture;
-            std::string filename = framesPath + "/frame_" + std::to_string(i) + ".png";
+            std::string filename = framesPath + "/frame-" + std::to_string(i) + ".png";
             if (!texture.loadFromFile(filename))
             {
                 std::cerr << "Erreur de chargement de l'image: " << filename << std::endl;
@@ -32,7 +30,6 @@ public:
     void run()
     {
         sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Lunar Pirate GUI", sf::Style::Fullscreen);
-        window.setFramerateLimit(m_frameRate);
 
         sf::Clock clock;
 
@@ -51,12 +48,19 @@ public:
                 }
             }
 
-            if (clock.getElapsedTime().asSeconds() > 1.0f / m_frameRate)
+            if (clock.getElapsedTime() > m_frameDuration)
             {
-                m_currentFrame = (m_currentFrame + 1) % m_frameCount;
+                m_currentFrame = (m_currentFrame + 1) % m_frames.size();
                 m_sprite.setTexture(m_frames[m_currentFrame]);
                 clock.restart();
             }
+
+            sf::Vector2u windowSize = window.getSize();
+            sf::Vector2u textureSize = m_frames[m_currentFrame].getSize();
+            m_sprite.setScale(
+                static_cast<float>(windowSize.x) / textureSize.x,
+                static_cast<float>(windowSize.y) / textureSize.y
+            );
 
             window.clear();
             window.draw(m_sprite);
@@ -68,13 +72,13 @@ private:
     std::vector<sf::Texture> m_frames;
     sf::Sprite m_sprite;
     int m_frameCount;
-    int m_frameRate;
+    sf::Time m_frameDuration;
     int m_currentFrame;
 };
 
 int main()
 {
-    Gui gui("frames", 10, 12);
+    Gui gui("../assets/graphisme/lunar_pirate.gif", 48, 30);
     gui.run();
 
     return 0;
