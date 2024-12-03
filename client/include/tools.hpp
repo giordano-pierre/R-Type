cd39f440085cb3405ff21a76a1ab39a87ef5f12d
Nathan Baudelin <nathan.baudelin@epitech.eu>
2024-12-03 22:03:04 +0100

    [ADD] useful structs

---
+++ client/include/tools.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** tools
+*/
+
+#pragma once
+
+namespace Rtype::Client {
+    struct TupleInt {
+        int x;
+        int y;
+    };
+
+    struct TupleFloat {
+        float x;
+        float y;
+    };
+}
