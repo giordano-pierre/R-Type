377792ef716dc6881378c9f18514179a2678d9aa
Nathan Baudelin <nathan.baudelin@epitech.eu>
2024-12-03 22:02:15 +0100

    [ADD] Input event

---
+++ client/include/events/InputEvent.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** InputEvent
+*/
+#pragma once
+
+#include <SFML/Window.hpp>
+
+namespace Rtype::Client {
+    struct InputEvent {
+        sf::Event event;
+    };
+}
