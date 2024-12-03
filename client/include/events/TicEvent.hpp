47a0513da3ad1125ccbba2b0e7a58dc052de58b8
Nathan Baudelin <nathan.baudelin@epitech.eu>
2024-12-03 22:02:33 +0100

    [ADD] Tic event

---
+++ client/include/events/TicEvent.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** TicEvent
+*/
+#pragma once
+
+#include <chrono>
+
+namespace timer = std::chrono;
+
+namespace Rtype::Client {
+    struct TicEvent {
+        TicEvent(const timer::time_point<timer::steady_clock> &time_stamp);
+        ~TicEvent() = default;
+
+        timer::time_point<timer::steady_clock> time_stamp;
+    };
+}
