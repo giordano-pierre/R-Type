78b8177ef3299b9416cba4365a80020ed8178c82
Nathan Baudelin <nathan.baudelin@epitech.eu>
2024-12-03 22:05:01 +0100

    [ADD] testing main

---
+++ client/src/client_main.cpp
@@ -0,0 +1,88 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** client_main
+*/
+#include <iostream>
+
+#include "ECS/ECS.hpp"
+
+#include "components/Position.hpp"
+#include "components/Velocity.hpp"
+#include "components/Drawable.hpp"
+#include "components/Playable.hpp"
+
+#include "events/FrameEvent.hpp"
+#include "events/InputEvent.hpp"
+#include "events/TicEvent.hpp"
+
+#include "systems/WindowSys.hpp"
+#include "systems/DrawableSys.hpp"
+#include "systems/VelocitySys.hpp"
+
+#include "TextureManager.hpp"
+
+int main (void) {
+    ECS ecs;
+    Rtype::Client::TextureManager myTextures;
+    ecs.register_component<Rtype::Client::Position>();
+    ecs.register_component<Rtype::Client::Velocity>();
+    ecs.register_component<Rtype::Client::Drawable>();
+    ecs.register_component<Rtype::Client::Playable>();
+
+    Entity entity1 = ecs.spawn_entity();
+    ecs.add_component<Rtype::Client::Position>(entity1, {{500, 500}, {500, 500}});
+    ecs.add_component<Rtype::Client::Velocity>(entity1, {{0, 0}});
+    ecs.add_component<Rtype::Client::Playable>(entity1, {1});
+    ecs.add_component<Rtype::Client::Drawable>(entity1, {myTextures.getTexture("assets/images/ship/red_ship.png"), {395, 250}, {395, 250}, 1, 0.1});
+
+    ecs.register_event<Rtype::Client::FrameEvent>();
+    ecs.register_event<Rtype::Client::InputEvent>();
+    ecs.register_event<Rtype::Client::TicEvent>();
+
+    auto windowSys = Rtype::Client::WindowSys({1920, 1080, 32}, "Test", sf::Style::Default);
+    ecs.subscribe<Rtype::Client::FrameEvent, Rtype::Client::Position, Rtype::Client::Drawable>(windowSys);
+
+    auto velocitySys = Rtype::Client::VelocitySys();
+    ecs.subscribe<Rtype::Client::TicEvent, Rtype::Client::Position, Rtype::Client::Velocity>(velocitySys);
+    ecs.subscribe<Rtype::Client::InputEvent, Rtype::Client::Playable, Rtype::Client::Velocity>(velocitySys);
+
+    auto drawableSys = Rtype::Client::DrawableSys();
+    ecs.subscribe<Rtype::Client::FrameEvent, Rtype::Client::Drawable>(drawableSys);
+    ecs.subscribe<Rtype::Client::InputEvent, Rtype::Client::Drawable>(drawableSys);
+
+    bool running = true;
+    ecs.subscribe<Rtype::Client::InputEvent>(
+        [&running](ECS &, const Rtype::Client::InputEvent &e_input) -> void {
+            if ((e_input.event.type == sf::Event::KeyPressed &&
+                e_input.event.key.code == sf::Keyboard::Escape) || e_input.event.type == sf::Event::Closed ){
+                running = false;
+            }
+        });
+
+    const auto FPS = 60;
+    const timer::duration<double, std::ratio<1, FPS>> frameRate(1);
+    timer::time_point<timer::steady_clock> frameStart;
+    timer::time_point<timer::steady_clock> newTime;
+    timer::duration<double> dtime = timer::duration<double>::zero();
+
+    while (running) {
+        newTime = timer::steady_clock::now();
+        dtime += newTime - frameStart;
+        frameStart = newTime;
+
+        if (dtime >= frameRate) {
+            ecs.post<Rtype::Client::FrameEvent>({std::chrono::steady_clock::now()});
+            ecs.post<Rtype::Client::TicEvent>({std::chrono::steady_clock::now()});
+
+            while (!ecs.empty()) {
+                auto evt = ecs.front();
+                evt();
+                ecs.pop_front();
+            }
+            dtime = std::chrono::duration<double>::zero();
+        }
+    }
+    return 0;
+}
