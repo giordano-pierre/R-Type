596abee9ae1f7a85456d77e3d7ae3e93a10e5c75
Nathan Baudelin <nathan.baudelin@epitech.eu>
2024-12-06 16:34:40 +0100

    [ADD] Type component

---
+++ client/include/components/Type.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Type
+*/
+
+#pragma once
+
+#include <ostream>
+
+#include "tools.hpp"
+
+namespace Rtype::Client {
+    struct Type {
+        ObjectType _type;
+    };
+}
+std::ostream &operator<<(std::ostream &out, const Rtype::Client::Type &type);
---
+++ client/src/components/Type.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Type
+*/
+
+#include "components/Type.hpp"
+
+std::ostream &operator<<(std::ostream &out, const Rtype::Client::Type &type)
+{
+    switch (type._type) {
+        case Rtype::Client::BACKGROUND:
+            out << "Is a Background";
+            break;
+        case Rtype::Client::PLAYER:
+            out << "Is a Player";
+            break;
+        case Rtype::Client::SHOT:
+            out << "Is a Shot";
+            break;
+        default:
+            break;
+    }
+    return out;
+}
