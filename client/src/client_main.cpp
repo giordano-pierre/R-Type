12351c9f9684d98c9c83466c7562dcafd93a3f38
Nathan Baudelin <nathan.baudelin@epitech.eu>
2024-12-09 20:47:50 +0100

    [UPD] new main for testing

--- client/src/client_main.cpp
+++ client/src/client_main.cpp
@@ -15,6 +15,7 @@
 #include "components/Type.hpp"
 #include "components/Hitbox.hpp"
 #include "components/Window.hpp"
+#include "components/Selectable.hpp"
 
 #include "events/FrameEvent.hpp"
 #include "events/InputEvent.hpp"
@@ -27,10 +28,11 @@
 #include "systems/BorderSys.hpp"
 #include "systems/ShootSys.hpp"
 #include "systems/CheatSys.hpp"
+#include "systems/SelectSys.hpp"
 
 #include "TextureManager.hpp"
 
-int main (void) {
+int mainTestGame (void) {
     ECS ecs;
     ecs.register_component<Rtype::Client::Window>();
     ecs.register_component<Rtype::Client::Type>();
@@ -91,6 +93,113 @@ int main (void) {
 
     auto shootSys = Rtype::Client::ShootSys();
     ecs.subscribe<Rtype::Client::InputEvent, Rtype::Client::Window, Rtype::Client::Playable, Rtype::Client::Position, Rtype::Client::Hitbox>(shootSys);
+
+    bool running = true;
+    ecs.subscribe<Rtype::Client::InputEvent>(
+        [&running](ECS &, const Rtype::Client::InputEvent &e_input) -> void {
+            if ((e_input._event.type == sf::Event::KeyPressed &&
+                e_input._event.key.code == sf::Keyboard::Escape) || e_input._event.type == sf::Event::Closed){
+                running = false;
+            }
+        });
+
+    auto windowSys = Rtype::Client::WindowSys({1920, 1080, 32}, "R-type", sf::Style::Titlebar | sf::Style::Close);
+    ecs.subscribe<Rtype::Client::FrameEvent, Rtype::Client::Window, Rtype::Client::Position, Rtype::Client::Hitbox, Rtype::Client::Drawable>(windowSys);
+
+    const auto FPS = 60;
+    const timer::duration<double, std::ratio<1, FPS>> frameRate(1);
+    timer::time_point<timer::steady_clock> frameStart;
+    timer::time_point<timer::steady_clock> newTime;
+    timer::duration<double> dtime = timer::duration<double>::zero();
+
+    while (running) {
+        newTime = timer::steady_clock::now();
+        dtime += newTime - frameStart;
+        frameStart = newTime;
+
+        if (dtime >= frameRate) {
+            ecs.post<Rtype::Client::TicEvent>({std::chrono::steady_clock::now()});
+            ecs.post<Rtype::Client::FrameEvent>({std::chrono::steady_clock::now()});
+
+            while (!ecs.empty()) {
+                auto evt = ecs.front();
+                evt();
+                ecs.pop_front();
+            }
+            dtime = std::chrono::duration<double>::zero();
+        }
+    }
+    return 0;
+}
+
+
+int main (void) {
+    ECS ecs;
+    ecs.register_component<Rtype::Client::Window>();
+    ecs.register_component<Rtype::Client::Type>();
+    ecs.register_component<Rtype::Client::Position>();
+    ecs.register_component<Rtype::Client::Velocity>();
+    ecs.register_component<Rtype::Client::Drawable>();
+    ecs.register_component<Rtype::Client::Playable>();
+    ecs.register_component<Rtype::Client::Hitbox>();
+    ecs.register_component<Rtype::Client::Selectable>();
+
+    Entity window = ecs.spawn_entity();
+    ecs.add_component<Rtype::Client::Type>(window, {Rtype::Client::WINDOW});
+    ecs.add_component<Rtype::Client::Window>(window, {{1280, 720}});
+    auto myWindow = ecs.get_components<Rtype::Client::Window>();
+
+    Entity button = ecs.spawn_entity();
+    ecs.add_component<Rtype::Client::Position>(button, {1280 / 2 - 400, 720 / 6});
+    ecs.add_component<Rtype::Client::Type>(button, {Rtype::Client::BUTTON});
+    ecs.add_component<Rtype::Client::Hitbox>(button, {{0.3, 0.1}});
+    ecs.add_component<Rtype::Client::Drawable>(button, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button.png"), {200, 30}, {200, 30}, 1});
+    ecs.add_component<Rtype::Client::Selectable>(button, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button_selected.png"), [](ECS ecs){std::cout << "Button 1 is clicked!" << std::endl;}});
+
+    Entity button2 = ecs.spawn_entity();
+    ecs.add_component<Rtype::Client::Position>(button2, {1280 / 2, 720 / 6});
+    ecs.add_component<Rtype::Client::Type>(button2, {Rtype::Client::BUTTON});
+    ecs.add_component<Rtype::Client::Hitbox>(button2, {{0.3, 0.1}});
+    ecs.add_component<Rtype::Client::Drawable>(button2, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button.png"), {200, 30}, {200, 30}, 1});
+    ecs.add_component<Rtype::Client::Selectable>(button2, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button_selected.png"), [](ECS ecs){std::cout << "Button 2 is clicked!" << std::endl;}});
+
+    Entity button3 = ecs.spawn_entity();
+    ecs.add_component<Rtype::Client::Position>(button3, {1280 / 2 + 400, 720 / 6});
+    ecs.add_component<Rtype::Client::Type>(button3, {Rtype::Client::BUTTON});
+    ecs.add_component<Rtype::Client::Hitbox>(button3, {{0.3, 0.1}});
+    ecs.add_component<Rtype::Client::Drawable>(button3, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button.png"), {200, 30}, {200, 30}, 1});
+    ecs.add_component<Rtype::Client::Selectable>(button3, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button_selected.png"), [](ECS ecs){std::cout << "Button 3 is clicked!" << std::endl;}});
+
+    Entity button4 = ecs.spawn_entity();
+    ecs.add_component<Rtype::Client::Position>(button4, {1280 / 2 - 400, 720 / 3});
+    ecs.add_component<Rtype::Client::Type>(button4, {Rtype::Client::BUTTON});
+    ecs.add_component<Rtype::Client::Hitbox>(button4, {{0.3, 0.1}});
+    ecs.add_component<Rtype::Client::Drawable>(button4, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button.png"), {200, 30}, {200, 30}, 1});
+    ecs.add_component<Rtype::Client::Selectable>(button4, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button_selected.png"), [](ECS ecs){std::cout << "Button 4 is clicked!" << std::endl;}});
+
+    Entity button5 = ecs.spawn_entity();
+    ecs.add_component<Rtype::Client::Position>(button5, {1280 / 2, 720 / 3});
+    ecs.add_component<Rtype::Client::Type>(button5, {Rtype::Client::BUTTON});
+    ecs.add_component<Rtype::Client::Hitbox>(button5, {{0.3, 0.1}});
+    ecs.add_component<Rtype::Client::Drawable>(button5, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button.png"), {200, 30}, {200, 30}, 1});
+    ecs.add_component<Rtype::Client::Selectable>(button5, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button_selected.png"), [](ECS ecs){std::cout << "Button 5 is clicked!" << std::endl;}});
+
+    Entity button6 = ecs.spawn_entity();
+    ecs.add_component<Rtype::Client::Position>(button6, {1280 / 2 + 400, 720 / 3});
+    ecs.add_component<Rtype::Client::Type>(button6, {Rtype::Client::BUTTON});
+    ecs.add_component<Rtype::Client::Hitbox>(button6, {{0.3, 0.1}});
+    ecs.add_component<Rtype::Client::Drawable>(button6, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button.png"), {200, 30}, {200, 30}, 1});
+    ecs.add_component<Rtype::Client::Selectable>(button6, {myWindow[0].value()._myTextures.getTexture("assets/images/utils/start_button_selected.png"), [](ECS ecs){std::cout << "Button 6 is clicked!" << std::endl;}});
+
+    ecs.register_event<Rtype::Client::FrameEvent>();
+    ecs.register_event<Rtype::Client::InputEvent>();
+    ecs.register_event<Rtype::Client::TicEvent>();
+
+    auto cheatSys = Rtype::Client::CheatSys();
+    ecs.subscribe<Rtype::Client::InputEvent, Rtype::Client::Window>(cheatSys);
+
+    auto selSys = Rtype::Client::SelectSys();
+    ecs.subscribe<Rtype::Client::InputEvent, Rtype::Client::Position, Rtype::Client::Drawable, Rtype::Client::Hitbox, Rtype::Client::Selectable>(selSys);
 
     bool running = true;
     ecs.subscribe<Rtype::Client::InputEvent>(
