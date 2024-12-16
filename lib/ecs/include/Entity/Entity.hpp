18f5b95657460354517daddd4c80002c6307740b
meksik <delya.meksi@epitech.eu>
2024-12-16 15:48:33 +0100

    [ADD] modifying ECS dependencies to add error handling and permanence

--- lib/ecs/include/Entity/Entity.hpp
+++ lib/ecs/include/Entity/Entity.hpp
@@ -12,11 +12,14 @@ class Entity
 {
     private:
         std::size_t _size;
+        std::bool _permanent;
 
     public:
-        explicit Entity(std::size_t size);
+        explicit Entity(std::size_t size, std::bool permanent = false);
         ~Entity() = default;
 
         void setSize(std::size_t size);
+        void setState(std::bool permanent);
         operator std::size_t() const;
+        bool isPermanent() const;
 };
