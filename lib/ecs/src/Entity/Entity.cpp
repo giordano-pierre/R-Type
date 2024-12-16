9ad9853351c22930974f189e7bc853ab558de858
meksik <delya.meksi@epitech.eu>
2024-12-16 15:50:04 +0100

    [ADD] modifying ECS dependencies to add entity permanence implementation

--- lib/ecs/src/Entity/Entity.cpp
+++ lib/ecs/src/Entity/Entity.cpp
@@ -7,9 +7,10 @@
 
 #include "Entity/Entity.hpp"
 
-Entity::Entity(std::size_t size)
+Entity::Entity(std::size_t size, std::bool permanent)
 {
     _size = size;
+    _permanent = permanent;
 }
 
 void Entity::setSize(std::size_t size)
@@ -17,7 +18,17 @@ void Entity::setSize(std::size_t size)
     _size = size;
 }
 
+void Entity::setState(std::bool permanent)
+{
+    _permanent = permanent;
+}
+
 Entity::operator std::size_t() const
 {
     return _size;
+}
+
+bool Entity::isPermanent()const
+{
+    return _permanent;
 }
