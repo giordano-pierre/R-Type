ef1dbdb87ebeb4c8081b47f3a7d562e53b529b44
Nellypal <nelly.pereira-alves@epitech.eu>
2024-12-16 14:47:57 +0100

    [ADD] submit server core

--- serveur/src/ServerCore.cpp
+++ serveur/src/ServerCore.cpp
@@ -1,19 +1,11 @@
-#include "server_header.h"
-#include "GameLogicSystem.hpp"
-#include "ScoreManager.hpp"
-class Server {
-private:
-    rtype::systems::GameLogicSystem gameLogicSystem;
-    float deltaTime;
+#include "ServerCore.hpp"
 
-public:
-    void run() {
-        while (true) {
-            update();
-        }
+Server::run() {
+    while (true) {
+        update();
     }
+}
 
-    void update() {
-        gameLogicSystem.update(deltaTime);
-    }
-};
+Server::update() {
+    gameLogicSystem.update(deltaTime);
+}
