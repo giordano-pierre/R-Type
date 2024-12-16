3545a2d9d12e337e8a3b6b1ca613cb0d33e5674b
meksik <delya.meksi@epitech.eu>
2024-12-16 16:02:50 +0100

    [ADD] movement system class and function implementation

---
+++ serveur/src/MovementSystem.cpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** MovementSystem.cpp
+*/
+
+#include "MovementSystem.hpp"
+
+namespace rtype {
+    namespace systems {
+
+        MovementSys::operator()(ECS &ecs,
+                            SparseArray<Position> &positions,
+                            const SparseArray<Velocity> &velocities) -> void
+        {
+            for (size_t i = 0; i < positions.size() && i < velocities.size(); ++i) {
+                if (positions[i].has_value() && velocities[i].has_value() && velocities[i]->active) {
+                    positions[i]->x += velocities[i]->x;
+                    positions[i]->y += velocities[i]->y;
+                }
+            }
+        }
+    }
+}
