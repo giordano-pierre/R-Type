f1f28a7f0389238817344adf17673e667cfef0c0
Nathan Baudelin <nathan.baudelin@epitech.eu>
2024-12-18 20:36:27 +0100

    [ADD] linked button for press one at the same time

--- client/src/systems/PressButtonSys.cpp
+++ client/src/systems/PressButtonSys.cpp
@@ -6,8 +6,21 @@
 */
 
 #include "systems/PressButtonSys.hpp"
+#include "buttonFunctions.hpp"
 
 namespace Rtype::Client {
+
+void checkLink(ECS &ecs, SparseArray<Pressable> &pressables, Entity ent) {
+  if (pressables[ent].value()._link == 0)
+    return;
+  int currentLink = pressables[ent].value()._link;
+  for (size_t i = 0; i < pressables.size(); ++i) {
+    auto &myPress = pressables[i];
+
+    if (myPress && myPress.value()._link == currentLink && myPress.value()._isPressed)
+      press(ecs, Entity(i));
+  }
+}
 
 void PressButtonSys::operator()(ECS &ecs, const InputEvent &e_input,
                                 const SparseArray<Selectable> &selectables,
@@ -18,8 +31,10 @@ void PressButtonSys::operator()(ECS &ecs, const InputEvent &e_input,
       const auto &sel = selectables[i];
       auto &press = pressables[i];
 
-      if (sel && press && sel.value()._isSelected)
+      if (sel && press && sel.value()._isSelected) {
+        checkLink(ecs, pressables, Entity(i));
         press.value()._press(ecs, Entity(i));
+      }
     }
     break;
   default:
@@ -49,8 +64,10 @@ void PressButtonSys::operator()(ECS &ecs, const InputEvent &e_input,
           e_input._event.mouseButton.y >
               pos.value()._client.y - (box.value()._client.y / 2) &&
           e_input._event.mouseButton.y <
-              pos.value()._client.y + (box.value()._client.y / 2))
+              pos.value()._client.y + (box.value()._client.y / 2)) {
+        checkLink(ecs, pressables, Entity(i));
         press.value()._press(ecs, Entity(i));
+      }
     }
   }
 }
