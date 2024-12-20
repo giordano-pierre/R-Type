88985cad9f394febecf0218f633fd60d6e648ad7
meksik <delya.meksi@epitech.eu>
2024-12-20 05:02:24 +0100

    [ADD] Health system declaration and implementation

---
+++ serveur/include/HealthSystem.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** HealthSystem.hpp
+*/
+
+#pragma once
+#include "Components.hpp"
+#include "ECS/ECS.hpp"
+
+namespace rtype::server {
+namespace systems {
+
+class HealthSys {
+  public:
+    HealthSys() = default;
+    ~HealthSys() = default;
+
+    void operator()(ECS &ecs, const rtype::server::TicEvent &,
+                    const SparseArray<Health> &healths,
+                    SparseArray<Basics> &basics);
+};
+} // namespace systems
+} // namespace rtype::server
