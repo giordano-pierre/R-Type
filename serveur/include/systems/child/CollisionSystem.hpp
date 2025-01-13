c9b590ae525d3a95f4200fe29abb6c4c616b4749
Nathan Baudelin <nathan.baudelin@epitech.eu>
2025-01-13 16:35:21 +0100

    [UPD] fix collision system

--- serveur/include/systems/child/CollisionSystem.hpp
+++ serveur/include/systems/child/CollisionSystem.hpp
@@ -10,22 +10,22 @@
 #include "ECS/ECS.hpp"
 #include "Events.hpp"
 
-namespace rtype::server::systems {
+namespace rtype::server {
 class CollisionSys {
   public:
     CollisionSys() = default;
     ~CollisionSys() = default;
 
-    void operator()(ECS &ecs, const rtype::server::TicEvent &,
+    void operator()(ECS &ecs, const TicEvent &,
                     const SparseArray<Position> &positions,
                     const SparseArray<HitBox> &hitboxes,
                     const SparseArray<Tag> &tags, SparseArray<Health> &healths,
-                    const SparseArray<PlayerData> &playersdata,
+                    const SparseArray<Owner> &owners,
                     SparseArray<Score> &scores);
 
-    void operator()(ECS &ecs, const rtype::server::TicEvent &,
+    void operator()(ECS &ecs, const TicEvent &,
                     SparseArray<Position> &positions,
                     const SparseArray<Tag> &tags,
                     const SparseArray<HitBox> &hitboxes);
 };
-} // namespace rtype::server::systems
+} // namespace rtype::server
