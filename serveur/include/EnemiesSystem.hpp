4b41e30da39518afe936e0c313a0ea20b672fad3
Nellypal <nelly.pereira-alves@epitech.eu>
2025-01-15 14:51:35 +0100

    [ADD] enemies system

---
+++ serveur/include/EnemiesSystem.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** EnemiesSystem.hpp
+*/
+
+#pragma once
+#include "Components.hpp"
+#include "ECS/ECS.hpp"
+#include <sol/sol.hpp>
+#include <string>
+
+namespace rtype::server {
+    namespace systems {
+        class EnemiesSys {
+        private:
+            sol::state lua;
+            std::string scriptsPath = "serveur/script/EnemiesComponant.lua";
+            std::unordered_map<size_t, float> entityTimes;
+
+            void initLua();
+            void registerComponents();
+            void loadBehaviors();
+            void updateBehavior(Position& pos, Velocity& vel, EnemyAI& ai, float dt);
+
+        public:
+            EnemiesSys();
+            ~EnemiesSys() = default;
+
+            void operator()(ECS& ecs, const rtype::server::TicEvent& tic,
+                SparseArray<Position>& positions,
+                const SparseArray<Tag>& tags,
+                SparseArray<EnemyAI>& ais,
+                SparseArray<Velocity>& velocities);
+        };
+    } // namespace systems
+} // namespace rtype::server
---
+++ serveur/src/EnemiesSystem.cpp
@@ -0,0 +1,140 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** EnemiesSystem.cpp
+*/
+
+#include "EnemiesSystem.hpp"
+
+namespace rtype::server {
+    namespace systems {
+        EnemiesSys::EnemiesSys() {
+        }
+
+        void EnemiesSys::initLua() {
+            lua.open_libraries(
+                sol::lib::base,
+                sol::lib::math,
+                sol::lib::table
+            );
+            registerComponents();
+            loadBehaviors();
+        }
+
+        void EnemiesSys::registerComponents() {
+            lua.new_enum<EnemyAI::BehaviorType>(
+                "BehaviorType",
+                {
+                    {"SINUSOIDAL", EnemyAI::BehaviorType::SINUSOIDAL},
+                    {"CIRCULAR", EnemyAI::BehaviorType::CIRCULAR},
+                    {"CHASE", EnemyAI::BehaviorType::CHASE},
+                    {"V_FORMATION", EnemyAI::BehaviorType::V_FORMATION},
+                    {"BOSS", EnemyAI::BehaviorType::BOSS}
+                }
+            );
+
+            // Exposer la struct EnemyAI avec accès direct aux membres
+            lua.new_usertype<EnemyAI>(
+                "EnemyAI",
+                "behaviorType", &EnemyAI::behaviorType,
+                "amplitude", &EnemyAI::amplitude,
+                "frequency", &EnemyAI::frequency,
+                "radius", &EnemyAI::radius,
+                "speed", &EnemyAI::speed,
+                "spacing", &EnemyAI::spacing,
+                "index", &EnemyAI::index
+            );
+
+            lua.new_usertype<Position>(
+                "Position",
+                "x", &Position::x,
+                "y", &Position::y
+            );
+
+            lua.new_usertype<Velocity>(
+                "Velocity",
+                "x", &Velocity::x,
+                "y", &Velocity::y
+            );
+        }
+        void EnemiesSys::loadBehaviors() {
+            try {
+                lua.script_file(scriptsPath);
+            }
+            catch (const sol::error& e) {
+                std::cerr << "Erreur lors du chargement des comportements: "
+                    << e.what() << std::endl;
+            }
+        }
+
+        void EnemiesSys::operator()(ECS& ecs, const rtype::server::TicEvent& tic,
+            SparseArray<Position>& positions,
+            const SparseArray<Tag>& tags,
+            SparseArray<EnemyAI>& ais,
+            SparseArray<Velocity>& velocities)
+        {
+            initLua();
+
+            for (size_t i = 0; i < positions.size(); ++i) {
+                if (!positions[i] || !tags[i] || !ais[i] || !velocities[i])
+                    continue;
+                if ((*tags[i]).type != EntityType::ENEMY1)
+                    continue;
+                auto& pos = *positions[i];
+                auto& ai = *ais[i];
+                auto& vel = *velocities[i];
+                static timer::time_point<timer::steady_clock> lastTic = tic.time_stamp;
+                float delta = std::chrono::duration<float>(tic.time_stamp - lastTic).count();
+                lastTic = tic.time_stamp;
+                updateBehavior(pos, vel, ai, delta);
+            }
+        }
+
+        void EnemiesSys::updateBehavior(Position& pos, Velocity& vel, EnemyAI& ai, float dt)
+        {
+            switch (ai.behaviorType) {
+            case EnemyAI::BehaviorType::SINUSOIDAL: {
+                sol::function update = lua["SineMovement"]["update"];
+                if (update.valid()) {
+                    sol::protected_function_result result = update(lua["SineMovement"], pos, vel, ai, dt);
+                    if (!result.valid()) {
+                        sol::error err = result;
+                        std::cerr << "Lua error: " << err.what() << std::endl;
+                    }
+                }
+                break;
+
+            }
+            case EnemyAI::BehaviorType::CIRCULAR: {
+                sol::function update = lua["CircleMovement"]["update"];
+                if (update.valid()) {
+                    update(pos, vel, ai, dt);
+                }
+                break;
+            }
+            case EnemyAI::BehaviorType::CHASE: {
+                sol::function update = lua["PlayerChase"]["update"];
+                if (update.valid()) {
+                    update(pos, vel, ai, dt);
+                }
+                break;
+            }
+            case EnemyAI::BehaviorType::V_FORMATION: {
+                sol::function update = lua["VFormation"]["update"];
+                if (update.valid()) {
+                    update(pos, vel, ai, dt);
+                }
+                break;
+            }
+            case EnemyAI::BehaviorType::BOSS: {
+                sol::function update = lua["BossBehavior"]["update"];
+                if (update.valid()) {
+                    update(pos, vel, ai, dt);
+                }
+                break;
+            }
+            }
+        }
+    } // namespace systems
+} // namespace rtype::server
