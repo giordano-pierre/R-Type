38cc20a5968ccf115b3aa1c7e891c8b023ec97fa
Nellypal <nelly.pereira-alves@epitech.eu>
2025-01-19 00:47:27 +0100

    [DEL] sigleton in child

--- serveur/src/Child.cpp
+++ serveur/src/Child.cpp
@@ -6,7 +6,7 @@
 */
 
 #include "Child.hpp"
-
+#include "systems/child/Singleton.hpp"
 namespace rtype::server {
 
 void initSubECS(ECS &ecs) {
@@ -21,6 +21,7 @@ void initSubECS(ECS &ecs) {
     ecs.register_component<Health>();
     ecs.register_component<Dead>();
     ecs.register_component<Owner>();
+    ecs.register_component<EnemyAI>();
 
     ecs.register_event<TicEvent>();
     ecs.register_event<RemoveClient>();
@@ -39,6 +40,8 @@ void loadSubGameSystem(Child &child) {
         .subscribe<TicEvent, Position, HitBox, Tag, Health, Owner, Score>(
             child._collSys);
     child._ecs_child.subscribe<TicEvent, Position, Tag, HitBox>(child._collSys);
+    auto enemiesSys = Singleton<EnemiesSys>();
+    child._ecs_child.subscribe<TicEvent, Position, Tag, EnemyAI, Velocity>(enemiesSys.getInstance());
 }
 
 } // namespace rtype::server
