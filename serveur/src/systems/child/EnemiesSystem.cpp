eaed617a340b4107fe3b0219c01026eeb68a044a
Nellypal <nelly.pereira-alves@epitech.eu>
2025-01-18 16:52:36 +0100

    [ADD] file for function enemysys

---
+++ serveur/src/systems/child/EnemiesSystem.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** EnemiesSystem.cpp
+*/
+
+#include "systems/child/EnemiesSystem.hpp"
+
+namespace rtype::server {
+namespace systems {
+EnemiesSys::EnemiesSys() { initLua(); }
+
+
+}
+} // namespace systems
+} // namespace rtype::server
