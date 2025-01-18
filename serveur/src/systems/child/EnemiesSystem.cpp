6517e01b9b1447bb5f5927ebdaa73ac33e58ffd5
Nellypal <nelly.pereira-alves@epitech.eu>
2025-01-18 16:55:04 +0100

    [ADD] register components

--- serveur/src/systems/child/EnemiesSystem.cpp
+++ serveur/src/systems/child/EnemiesSystem.cpp
@@ -16,6 +16,26 @@ void EnemiesSys::initLua() {
     registerComponents();
     loadBehaviors();
 }
+void EnemiesSys::registerComponents() {
+    lua.new_enum<EnemyAI::BehaviorType>(
+        "BehaviorType", { {"SINUSOIDAL", EnemyAI::BehaviorType::SINUSOIDAL},
+                         {"CIRCULAR", EnemyAI::BehaviorType::CIRCULAR},
+                         {"CHASE", EnemyAI::BehaviorType::CHASE},
+                         {"V_FORMATION", EnemyAI::BehaviorType::V_FORMATION},
+                         {"BOSS", EnemyAI::BehaviorType::BOSS} });
+
+    lua.new_usertype<EnemyAI>("EnemyAI", "behaviorType", &EnemyAI::behaviorType,
+        "amplitude", &EnemyAI::amplitude, "frequency",
+        &EnemyAI::frequency, "radius", &EnemyAI::radius,
+        "speed", &EnemyAI::speed, "spacing",
+        &EnemyAI::spacing, "index", &EnemyAI::index);
+
+    lua.new_usertype<Position>("Position", "x", &Position::x, "y",
+        &Position::y);
+
+    lua.new_usertype<Velocity>("Velocity", "x", &Velocity::x, "y",
+        &Velocity::y);
+}
 
 } // namespace systems
 } // namespace rtype::server
