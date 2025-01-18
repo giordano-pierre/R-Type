b8e1b498c2c22ca2e8f0b2e6ec28a38629358031
Nellypal <nelly.pereira-alves@epitech.eu>
2025-01-18 16:36:58 +0100

    [ADD] function mouv sinosoidale

--- serveur/script/EnemiesComponant.lua
+++ serveur/script/EnemiesComponant.lua
@@ -8,3 +8,11 @@
 SineMovement = {
     time = 0
 }
+-- add gestion d'eereur division par 0
+function SineMovement:update(position, velocity, ai, dt)
+    self.time = (self.time or 0) + dt
+    newY = ai.amplitude * math.sin(ai.frequency * self.time)
+    position.y = math.floor(position.y + newY)
+    position.x = math.floor(position.x - velocity.x)
+end
+
