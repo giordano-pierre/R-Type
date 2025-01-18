b3721a17bccafb3a3243508aa2ca26be19d3942e
Nellypal <nelly.pereira-alves@epitech.eu>
2025-01-18 16:54:18 +0100

    [ADD] init lua

--- serveur/src/systems/child/EnemiesSystem.cpp
+++ serveur/src/systems/child/EnemiesSystem.cpp
@@ -11,7 +11,11 @@ namespace rtype::server {
 namespace systems {
 EnemiesSys::EnemiesSys() { initLua(); }
 
-
+void EnemiesSys::initLua() {
+    lua.open_libraries(sol::lib::base, sol::lib::math, sol::lib::table);
+    registerComponents();
+    loadBehaviors();
 }
+
 } // namespace systems
 } // namespace rtype::server
