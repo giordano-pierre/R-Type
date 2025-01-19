85f149692df161dac67e6f9958798e719e5bb4de
Nathan Baudelin <nathan.baudelin@epitech.eu>
2025-01-19 17:14:26 +0100

    [REFACTO] split window component

---
+++ client/include/components/Configs.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** Configs
+*/
+
+#pragma once
+
+#include "clientTools.hpp"
+
+namespace rtype::client {
+
+struct Configs {
+    Configs(TupleUInt size, TupleUInt serverSize);
+
+    TupleUInt _size;
+    TupleUInt _serverSize;
+    std::pair<std::map<UserInput, std::pair<sf::Keyboard::Key,
+                                            std::shared_ptr<std::string>>>,
+              std::map<UserInput, sf::Keyboard::Key>>
+        _inputConfig;
+    std::string _lang = "EN";
+    bool _colorblind = false;
+    bool _displayHitboxs = false;
+};
+
+}
---
+++ client/include/components/PlayerInfo.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** PlayerInfo
+*/
+
+#pragma once
+
+#include <string>
+
+namespace rtype::client {
+
+struct PlayerInfo {
+    int _nbPlayer = 1;
+    std::string _name1 = "player1";
+    std::string _color1 = "red";
+    std::string _name2 = "player2";
+    std::string _color2 = "red";
+};
+
+}
