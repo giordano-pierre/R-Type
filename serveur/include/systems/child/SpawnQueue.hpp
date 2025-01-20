a0c7e6db831f23dd3912b281b0723d00649f1363
Nellypal <nelly.pereira-alves@epitech.eu>
2025-01-20 09:55:35 +0100

    [ADD] spawnqueue

---
+++ serveur/include/systems/child/SpawnQueue.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Singleton.hpp
+*/
+
+#pragma once
+
+#include "Components.hpp"
+#include <vector>
+#include <stdexcept>
+
+class SpawnQueue {
+public:
+    void add(const rtype::server::EnemyAI::BehaviorType& behavior) {
+        _spawnQueue.push_back(behavior);
+    }
+
+    rtype::server::EnemyAI::BehaviorType pop() {
+        if (_spawnQueue.empty()) {
+            throw std::runtime_error("SpawnQueue is empty. Cannot pop.");
+        }
+        rtype::server::EnemyAI::BehaviorType front = _spawnQueue.front();
+        _spawnQueue.erase(_spawnQueue.begin());
+        return front;
+    }
+
+    bool isEmpty() const {
+        return _spawnQueue.empty();
+    }
+
+private:
+    std::vector<rtype::server::EnemyAI::BehaviorType> _spawnQueue;
+};
