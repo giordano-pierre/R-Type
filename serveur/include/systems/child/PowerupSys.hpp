fa0605625a7f1f51b4af948c5f44a98f89c449f2
ustrafeev <irina.ustrafeeva@epitech.eu>
2025-01-21 07:19:05 +0100

    [UPD] correct size for powerups

--- serveur/include/systems/child/PowerupSys.hpp
+++ serveur/include/systems/child/PowerupSys.hpp
@@ -22,7 +22,7 @@ class PowerupSys {
         ecs.add_component<Position>(powerup, {event._x, event._y});
         ecs.add_component<Powerup>(powerup, {event._type, event._duration});
         ecs.add_component<Tag>(powerup, {fetch_new_uuid(), POWERUP});
-        ecs.add_component<HitBox>(powerup, HitBox(0.115, 0.120));
+        ecs.add_component<HitBox>(powerup, HitBox(0.040, 0.060));
         ecs.add_component<Velocity>(powerup, Velocity(-1, 0));
 
         std::cout << "pup at " << event._x << ", " << event._y
